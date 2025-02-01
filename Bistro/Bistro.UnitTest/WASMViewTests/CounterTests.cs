4e1365b5d69e4912f65351c2d410f347efa02a41
George Lungoci <cristi.lungoci16@gmail.com>
2025-01-31 18:12:06 -0600

    BIS-4: Deleted empty lines and used constant variables.

--- Bistro/Bistro.UnitTest/WASMViewTests/CounterTests.cs
+++ Bistro/Bistro.UnitTest/WASMViewTests/CounterTests.cs
@@ -9,18 +9,14 @@ namespace Bistro.UnitTest.WASMViewTests
         public void CounterShouldIncrementWhenClicked()
         {
             // Arrange
-
             using var ctx = new TestContext();
             var rc = ctx.RenderComponent<Counter>();
-            
 
             // Act
-
-            rc.Find("button").Click();
-
+            rc.Find(Constants.Button).Click();
 
             // Assert
-            var counterDisplay = rc.Find("p").TextContent;
+            var counterDisplay = rc.Find(Constants.Paragraph).TextContent;
             counterDisplay.MarkupMatches("Current count: 1");
         }
     }
