f4962979f090cb81c1b04df15092c7642ac62379
glungo16 <138644033+glungo16@users.noreply.github.com>
2025-02-03 10:27:53 -0500

    BIS-4: Create a basic unit test (#6)
    * BIS-2 Added new Blazor project with docker support
    
    * BIS-2 Added Project with Docker Support
    
    * BIS-3 Structured the project in layers based on EBI architecture.
    
    * BIS-2 Added Project with Docker Support
    
    * BIS-3 Structured the project in layers based on EBI architecture.
    
    * BIS-3: Rebase main
    
    * BIS-4: Created basic Unit Test
    
    * BIS-4: Deleted empty lines and used constant variables.

---
+++ Bistro/Bistro.UnitTest/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bistro.UnitTest
+{
+    public static class Constants
+    {
+        public static readonly string Button = "button";
+        public static readonly string Paragraph = "p";
+    }
+}
